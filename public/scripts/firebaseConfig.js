35b5c6ba4f3117e046da64a5915af40a257d45de
Evandro Mateus <evandromateus06@gmail.com>
2021-01-10 19:42:31 -0300

    autenticaçao

---
+++ public/scripts/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDt4_GUABBZizn7sAik9ga3WrKzcYMVRXs",
+  authDomain: "authenticationevents.firebaseapp.com",
+  projectId: "authenticationevents",
+  storageBucket: "authenticationevents.appspot.com",
+  messagingSenderId: "820050316302",
+  appId: "1:820050316302:web:51d49352eb0254cf18005f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ public/scripts/page-auth.js
@@ -0,0 +1,34 @@
+let inputEmail = document.querySelector('#email')
+let inputPassword = document.querySelector('#password')
+let inputName = document.querySelector('#name')
+let btnRegister = document.querySelector('#register')
+let btnLogin = document.querySelector('#login')
+const auth = firebase.auth()
+
+btnRegister.addEventListener('click', () => {
+    auth.createUserWithEmailAndPassword(inputEmail.value, inputPassword.value).then(res => {
+
+        const user = firebase.auth().currentUser
+        
+        user.updateProfile({
+            displayName: inputName.value
+        })
+
+        console.log(res)
+
+    }).catch(error => {
+        console.log(error.code)
+        console.log(error.message)
+    })
+})
+
+btnLogin.addEventListener('click', () => {
+    auth.signInWithEmailAndPassword(inputEmail.value, inputPassword.value).then(user => {
+        alert("Você está autenticado.")
+        alert(user.displayName)
+        location.replace('/')
+    }).catch(error => {
+        console.log(error.code)
+        console.log(error.message)
+    })
+})
